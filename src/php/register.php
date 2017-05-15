354d03bda1c6bad8f55469c41d6c452b768cc46b
dagou <644537820@qq.com>
2017-05-15 08:58:56 +0800

    星期一早上更新

--- src/php/register.php
+++ src/php/register.php
@@ -5,18 +5,18 @@
 	$password = $_POST["password"];
 	$apassword = $_POST["apassword"];
 	//判断当前 username 是否已存在数据表中
-	$usernameCheck = "select * from registersh where `username` ='$username'";
+	$usernameCheck = "select * from shop_register where `username` ='$username'";
 	$result = query($usernameCheck);
 
 	//当前 email 不存在，执行插入操作
 	if(count($result) < 1){
-		$sql = "insert into registersh(username, password, apassword,yan) values('$username', '$password', '$apassword')";
+		$sql = "insert into shop_register(username, password, apassword) values('$username', '$password', '$apassword')";
 		// echo $sql;
 		$excute = excute($sql);
 		if($excute){
 			echo "{state: true}";
 		} else {
-			echo "{state: false, message: '插入失败！！！'}";
+			echo "{state: false, message: '注册失败！！！'}";
 		}
 	} else {
 		echo "{state: false, message: '登录名 已被注册！！！'}";
